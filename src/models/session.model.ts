e68edfb35eb74fe03932820d73ac8f2d51e66d01
Charles-Hébert Dalzon <charlby5@gmail.com>
2022-09-05 16:34:00 -0400

    Create the Session Model

---
+++ src/models/session.model.ts
@@ -0,0 +1,35 @@
+import mongoose, { Document } from 'mongoose';
+
+import { UserDocument } from './user.model';
+
+export interface SessionDocument extends Document {
+  user: UserDocument['_id'];
+  valid: boolean;
+  userAgent: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const SessionSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    valid: {
+      type: Boolean,
+      default: true,
+    },
+    userAgent: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Session = mongoose.model('Session', SessionSchema);
+
+export default Session;
