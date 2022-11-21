4bd91e2e44a0bcad14343fcaf60f4d45b9d011c6
Dalzon Charles-Hébert <charlby5@gmail.com>
2022-11-20 21:51:07 -0500

    Get metrics for rest api response time

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 require('dotenv').config();
 import config from 'config';
 import cors from 'cors';
+import responseTime from 'response-time';
 
 import connect from './utils/connect';
 import log from './utils/logger';
 import routes from './routes';
 import deserializeUser from './middleware/deserializeUser';
 import swaggerDocs from './utils/swagger';
-import { startMetricServer } from './utils/metrics';
+import { restResponseTimeHistogram, startMetricServer } from './utils/metrics';
 
 const app = express();
 
@@ -16,6 +17,20 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(deserializeUser);
+app.use(
+  responseTime((req: Request, res: Response, time: number) => {
+    if (req.route.path) {
+      restResponseTimeHistogram.observe(
+        {
+          method: req.method,
+          status_code: res.statusCode,
+          route: req.route.path,
+        },
+        time * 1000
+      );
+    }
+  })
+);
 
 // routes
 routes(app);
