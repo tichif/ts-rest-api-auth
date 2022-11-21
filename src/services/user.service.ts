ceca4acdbd5c24de43d465845d904605bcb1a1ad
Dalzon Charles-Hébert <charlby5@gmail.com>
2022-11-20 22:00:47 -0500

    Get metrics when creating user

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,15 +2,23 @@ import { DocumentDefinition, FilterQuery } from 'mongoose';
 import { omit } from 'lodash';
 
 import User, { UserDocument } from '../models/user.model';
+import { databaseResponseTimeHistogram } from '../utils/metrics';
 
 export async function createUser(
   input: DocumentDefinition<
     Omit<UserDocument, 'createdAt' | 'updatedAt' | 'comparePassword'>
   >
 ) {
+  const timer = databaseResponseTimeHistogram.startTimer();
+  const metricsLabels = {
+    operation: 'createUser',
+  };
   try {
-    return await User.create(input);
+    const result = await User.create(input);
+    timer({ ...metricsLabels, success: 1 });
+    return result;
   } catch (error: any) {
+    timer({ ...metricsLabels, success: 0 });
     throw new Error(error);
   }
 }
